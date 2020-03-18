dfefc7fb6fdf889245e7d7e2da02620b8b1b32bc
amirisback <faisalamircs@gmail.com>
2020-03-19 01:32:34 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#
+# Created by Faisal Amir
+# FrogoBox Inc License
+# -----------------------------------------
+# TensorFlow-Learning
+# Copyright (C) 18/03/2020.      
+# All rights reserved
+# -----------------------------------------
+# Name     : Muhammad Faisal Amir
+# E-mail   : faisalamircs@gmail.com
+# Github   : github.com/amirisback
+# LinkedIn : linkedin.com/in/faisalamircs
+# -----------------------------------------
+# FrogoBox Software Industries
+# 
+# /
+
